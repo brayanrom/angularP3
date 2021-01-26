3f999e7fc4b4c46781612b65d3529464e258d620
BrayanRomero <brayanitai1634@gmail.com>
2021-01-25 23:18:09 -0600

    Subida de todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EdadGuard} from './edad.guard'
+import {FirstComponent} from './first/first.component';
+import {SecondComponent} from './second/second.component';
+import{NotfoundComponent} from './notfound/notfound.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'first',component:FirstComponent},
+  {path:'second',component:SecondComponent,canActivate:[EdadGuard]},
+  {path:'**',component:NotfoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
